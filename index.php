9e87100a597894603610112551ca60d6e5c1da2f
RomanNikolaevich <85992849+RomanNikolaevich@users.noreply.github.com>
2022-04-01 16:59:40 +0300

    14 урок - закончил изучение

--- index.php
+++ index.php
@@ -32,36 +32,7 @@ header('Content-Type: text/html; charset=utf-8');
 </head>
 <body>
 <?php
-if(isset($_POST['login'], $_POST['pass'])) {
-	echo $_POST['login'];
-	//echo 'GET: <pre>'.print_r($_GET, 1).'</pre>';
-	echo 'POST: <pre>'.print_r($_POST, 1).'</pre>';
-	if(isset($_POST['like'])) {
-		echo 'Человек любит'.$_POST['like'][0];
-	} else {
-		echo 'Человек ничего не любит';
-	} echo "Вы успешно зарегистрировались";
-} else {// если регистрация не прошла, то выполняется блок кода расположеного ниже - скобка открылась
+
 ?>
-<h1>Форма</h1>
-<form action="" method="post">
-	<input type="hidden" name="hide" value="скрытое значение">
-	<div>Заполните логин:<input type="text" name="login" value=""></div>
-	<div>Заполните пароль:<input type="password" name="pass" value=""></div>
-	<div style="padding:15px">
-		Пол: <br>
-		<label>Мужчина <input type="radio" name="gender" value="Мужчина"></label> |
-		<label>Женщина <input type="radio" name="gender" value="Женщина"></label>
-	</div>
-	<div style="padding:15px">
-		Любимый напиток: <br>
-		<label>Кофе <input type="checkbox" name="like[]" value="Кофе"></label> |
-		<label>Чай <input type="checkbox" name="like[]" value="Чай"></label> |
-		<label>Молоко <input type="checkbox" name="like[]" value="Молоко"></label>
-	</div>
-	<div><input type="submit" name="submit" value="Отправить данные" style="border-style:hidden; border-radius:10px;
-	background-color:#90ff90"></div>
-</form>
-<?php } ?><!-- скобка закрылась-->
 </body>
 </html>
